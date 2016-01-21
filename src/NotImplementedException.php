a09f02f120ceac55980aa3b3dd65fdb6e241be21
Wes Gilleland <wes.gilleland@gmail.com>
2016-01-20 22:29:09 -0500

    Initial Commit

---
+++ src/NotImplementedException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wes
+ * Date: 1/20/2016
+ * Time: 10:27 PM
+ */
+
+namespace Deathnerd\WTForms;
+
+
+use Exception;
+
+class NotImplementedException extends Exception
+{
+
+}
