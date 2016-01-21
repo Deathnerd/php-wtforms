429d844dccc68310110a3c2628cd9bbe9777f0af
Wes Gilleland <wes.gilleland@gmail.com>
2016-01-21 11:01:39 -0500

    Added a todo

--- src/helpers.php
+++ src/helpers.php
@@ -148,6 +148,7 @@ if(!function_exists('chain')){
 }
 
 abstract class CanCall {
+    // TODO Possibly get rid of this and implement a Callable structure
     public function call(){
         throw new RuntimeException;
     }
