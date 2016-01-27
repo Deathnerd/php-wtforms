f48f4ed92f63fabc72ff44f33a011a84fb039f2f
Wes Gilleland <wgilleland@elinkdesign.com>
2016-01-27 17:57:44 -0500

    Initial Commit

---
+++ src/FilterInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wes Gilleland
+ * Date: 1/27/2016
+ * Time: 4:35 PM
+ */
+
+namespace Deathnerd\WTForms;
+
+
+interface FilterInterface
+{
+    public static function run($data);
+}
