6a4eda35046fa6bb7a4123b1c120fa1d95805a8e
Wes Gilleland <wes.gilleland@gmail.com>
2016-01-28 11:11:01 -0500

    Initial Commit

---
+++ src/fields/RadioField.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wes
+ * Date: 1/28/2016
+ * Time: 11:04 AM
+ */
+
+namespace Deathnerd\WTForms\Fields;
+
+use Deathnerd\WTForms\Widgets\ListWidget;
+use Deathnerd\WTForms\Widgets\RadioInput;
+
+/**
+ * Like a SelectField, except displays a list of radio buttons.
+ *
+ * Iterating a field will produce subfields (each containing a label as
+ * well) in order to allow custom rendering of the individual radio fields.
+ *
+ * @package Deathnerd\WTForms\Fields
+ */
+class RadioField extends SelectField
+{
+    /**
+     * @inheritdoc
+     */
+    public function __construct($label = "", array $validators = [], array $choices = [], array $kwargs = [])
+    {
+        parent::__construct($label, $validators, $choices, $kwargs);
+        $this->option_widget = new RadioInput();
+        $this->widget = new ListWidget("ul", false);
+    }
+}
---
+++ src/widgets/RadioInput.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wes
+ * Date: 1/28/2016
+ * Time: 11:07 AM
+ */
+
+namespace Deathnerd\WTForms\Widgets;
+
+
+class RadioInput extends Widget
+{
+
+}
