abe3d42c1e40f3fdc9448a4f0bef512c02120dba
Wes Gilleland <wes.gilleland@gmail.com>
2016-01-28 13:47:05 -0500

    Initial Commit of FloatField

---
+++ src/fields/FloatField.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wes
+ * Date: 1/28/2016
+ * Time: 1:40 PM
+ */
+
+namespace Deathnerd\WTForms\Fields;
+
+
+use Deathnerd\WTForms\ValueError;
+use Deathnerd\WTForms\Widgets\TextInput;
+
+class FloatField extends Field
+{
+    /**
+     * FloatField constructor.
+     * @param string $label
+     * @param array $kwargs
+     */
+    public function __construct($label = "", array $kwargs = [])
+    {
+        parent::__construct($label, $kwargs);
+        $this->widget = new TextInput();
+    }
+
+    public function _value()
+    {
+        if ($this->raw_data) {
+            return $this->raw_data[0];
+        } elseif ($this->data !== null) {
+            return strval($this->data);
+        }
+        return "";
+    }
+
+    public function process_formdata(array $valuelist)
+    {
+        if(!empty($valuelist)){
+            if(is_numeric($valuelist[0])){
+                $this->data = floatval($valuelist[0]);
+            } else {
+                $this->data = null;
+                throw new ValueError($this->gettext("Not a valid float value"));
+            }
+        }
+    }
+}
