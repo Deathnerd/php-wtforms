fe627c4580a53411a344823901c490610368ed23
Wes Gilleland <wes.gilleland@gmail.com>
2016-01-27 22:55:36 -0500

    Initial Commit

---
+++ src/validators/MacAddress.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wes
+ * Date: 1/27/2016
+ * Time: 10:45 PM
+ */
+
+namespace Deathnerd\WTForms\Validators;
+
+
+use Deathnerd\WTForms\BaseForm;
+use Deathnerd\WTForms\Fields\Field;
+
+/**
+ * Validates a MAC Address
+ * @package Deathnerd\WTForms\Validators
+ */
+class MacAddress extends Validator
+{
+    /**
+     * MacAddress constructor.
+     * @param string $message Error message to raise in case of a validation error.
+     */
+    public function __construct($message = "")
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @param BaseForm $form
+     * @param Field $field
+     * @return mixed
+     * @throws ValidationError
+     */
+    function __invoke(BaseForm $form, Field $field)
+    {
+        $value = $field->data;
+        $valid = false;
+        if (!is_null($value)) {
+            $valid = filter_var($value, FILTER_VALIDATE_MAC);
+        }
+        if (!$valid) {
+            $message = $this->message;
+            if ($message == "") {
+                $message = $field->gettext("Invalid MAC address.");
+            }
+            throw new ValidationError($message);
+        }
+    }
+}
