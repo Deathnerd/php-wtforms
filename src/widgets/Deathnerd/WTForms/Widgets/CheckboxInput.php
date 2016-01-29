972e1b6a63fce58f8c66b98cb5e6b63e64dbba7d
Wes Gilleland <wes.gilleland@gmail.com>
2016-01-28 21:12:54 -0500

    Dummy Skeleton for Fields. Need to flesh out

---
+++ src/widgets/Deathnerd/WTForms/Widgets/CheckboxInput.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wes
+ * Date: 1/28/2016
+ * Time: 9:09 PM
+ */
+
+namespace Deathnerd\WTForms\Widgets;
+
+
+class CheckboxInput
+{
+
+    /**
+     * CheckboxInput constructor.
+     */
+    public function __construct()
+    {
+    }
+}
