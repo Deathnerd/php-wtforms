bea065168172b1595009cefd1da1f57e6854c634
Wes Gilleland <wgilleland@elinkdesign.com>
2016-02-02 15:45:28 -0500

    Initial commit for DateTimeLocalField

---
+++ src/fields/html5/DateTimeLocalField.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wes Gilleland
+ * Date: 2/2/2016
+ * Time: 3:22 PM
+ */
+
+namespace Deathnerd\WTForms\Fields\HTML5;
+use Deathnerd\WTForms\Fields\Core\DateTimeField;
+use Deathnerd\WTForms\Widgets\HTML5\DateTimeLocalInput;
+
+/**
+ * Represents an ``<input type="datetime-local">``.
+ * @package Deathnerd\WTForms\Fields\HTML5
+ */
+class DateTimeLocalField extends DateTimeField
+{
+    /**
+     * @inheritdoc
+     */
+    public function __construct($label, array $kwargs)
+    {
+        parent::__construct($label, $kwargs);
+        $this->widget = new DateTimeLocalInput();
+    }
+
+}
