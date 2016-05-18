9cd8fd437ec7136efad985a7e6e3403d136d7281
Wes Gilleland <wgilleland@elinkdesign.com>
2016-05-18 13:00:24 -0400

    First test for FieldList done

--- tests/fields/FieldListTest.php
+++ tests/fields/FieldListTest.php
@@ -12,10 +12,26 @@ namespace WTForms\Tests\Fields;
 use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Doctrine\Common\Annotations\FileCacheReader;
-use WTForms\Form;
+use WTForms\Annotations\Fields\Core\FieldList;
+use WTForms\Annotations\Fields\Core\StringField;
+use WTForms\Annotations\Form;
+use WTForms\Annotations\Validators\DataRequired;
 use WTForms\Forms;
 use WTForms\Tests\SupportingClasses\AnnotatedHelper;
 use WTForms\Tests\SupportingClasses\Helper;
+
+/**
+ * @Form
+ */
+class FieldListTestForm
+{
+  /**
+   * @var FieldList
+   * @FieldList(inner_field=@StringField(validators={@DataRequired}))
+   */
+  public $a;
+}
+
 
 class FieldListTest extends \PHPUnit_Framework_TestCase
 {
@@ -23,9 +39,7 @@ class FieldListTest extends \PHPUnit_Framework_TestCase
   protected $annotated_helper;
   protected $registry;
   protected $reader;
-  /**
-   * @var Form
-   */
+
   protected $form;
 
   public function setUp()
@@ -44,20 +58,39 @@ class FieldListTest extends \PHPUnit_Framework_TestCase
   public function testForm()
   {
     $data = ['foo', 'hi', 'rawr'];
-    $this->form = Forms::create($this->annotated_helper, [], ["list_of_fields" => $data]);
-    $list_of_fields = $this->form->list_of_fields;
-    $this->assertEquals("hi", $list_of_fields->entries[1]->data);
-    $this->assertEquals("list_of_fields-1", $list_of_fields->entries[1]->name);
-    $this->assertEquals(count($data), count(array_intersect($list_of_fields->data, $data)));
-    $this->assertEquals(3, count($list_of_fields->entries));
+    $this->form = Forms::create(new FieldListTestForm, [], ["a" => $data]);
+    $a = $this->form->a;
+    $this->assertEquals("hi", $a->entries[1]->data);
+    $this->assertEquals("a-1", $a->entries[1]->name);
+    $this->assertEquals(count($data), count(array_intersect($a->data, $data)));
+    $this->assertEquals(3, count($a->entries));
 
-    $post_data = ["list_of_fields-0" => ["bleh"],
-                  "list_of_fields-3" => ["yarg"],
-                  "list_of_fields-4" => [""],
-                  "list_of_fields-7" => ["mmm"]];
-    $this->form = Forms::create($this->annotated_helper, $post_data);
-    $this->assertEquals(4, count($this->form->list_of_fields->entries));
-    $this->assertEquals(["bleh", "yarg", "", "mmm"], $this->form->list_of_fields->data);
-    
+    $post_data = ["a-0" => ["bleh"],
+                  "a-3" => ["yarg"],
+                  "a-4" => [""],
+                  "a-7" => ["mmm"]];
+    $this->form = Forms::create(new FieldListTestForm, $post_data);
+    $this->assertEquals(4, count($this->form->a->entries));
+    $this->assertEquals(["bleh", "yarg", "", "mmm"], $this->form->a->data);
+    $this->assertFalse($this->form->validate());
+
+
+    $this->form = Forms::create(new FieldListTestForm, $post_data, ["a" => $data]);
+    $this->assertEquals(["bleh", "yarg", "", "mmm"], $this->form->a->data);
+    $this->assertFalse($this->form->validate());
+
+    // test for formdata precedence
+    $post_data = ["a-0" => ["a"], "a-1" => ["b"]];
+    $this->form = Forms::create(new FieldListTestForm, $post_data, ["a" => $data]);
+    $this->assertEquals(2, count($this->form->a->entries));
+    $this->assertEquals($this->form->a->data, ["a", "b"]);
+    $expected = [];
+    // Testing iteration
+    foreach ($this->form->a as $entry) {
+      $expected[] = $entry;
+    }
+    $this->assertEquals(count($expected), count($this->form->a->entries));
+    // Testing countable
+    $this->assertEquals(count($this->form->a), 2);
   }
 }
