fc5ee4be269ef64d7667d537a666af91c8926158
Wes Gilleland <wes.gilleland@gmail.com>
2016-05-29 17:56:44 -0400

    Validators reporting all 100% coverage

---
+++ tests/validators/URLTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wes
+ * Date: 5/29/2016
+ * Time: 5:34 PM
+ */
+
+namespace WTForms\Tests\Validators;
+
+
+use WTForms\Form;
+use WTForms\Tests\SupportingClasses\DummyField;
+use WTForms\Validators\URL;
+
+class URLTest extends \PHPUnit_Framework_TestCase
+{
+  public $form;
+  public $url;
+
+  protected function setUp()
+  {
+    $this->form = new Form();
+    $this->url = new URL;
+  }
+
+  public function testURL()
+  {
+    $this->assertNull($this->url->__invoke($this->form, new DummyField(["data" => "http://foobar.dk"])));
+    $this->assertNull($this->url->__invoke($this->form, new DummyField(["data" => "http://foobar.dk/"])));
+    $this->assertNull($this->url->__invoke($this->form, new DummyField(["data" => "http://foobar.museum/foobar"])));
+    $this->assertNull($this->url->__invoke($this->form, new DummyField(["data" => "http://127.0.0.1/foobar"])));
+    $this->assertNull($this->url->__invoke($this->form, new DummyField(["data" => "http://127.0.0.1:9000/fake"])));
+    $this->assertNull($this->url->__invoke($this->form, new DummyField(["data" => "http://localhost/foobar"])));
+    $this->assertNull($this->url->__invoke($this->form, new DummyField(["data" => "http://foobar"])));
+  }
+
+  /**
+   * @expectedException \WTForms\Exceptions\ValidationError
+   * @expectedExceptionMessage foo
+   */
+  public function testNoProtocolAndCustomMessage()
+  {
+    $this->url = new URL("foo");
+    $this->url->__invoke($this->form, new DummyField(["data" => "foobar.dk"]));
+  }
+
+}
---
+++ tests/validators/ValidatorTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wes
+ * Date: 5/29/2016
+ * Time: 5:48 PM
+ */
+
+namespace WTForms\Tests\Validators;
+
+
+use WTForms\Form;
+use WTForms\Tests\SupportingClasses\DummyField;
+use WTForms\Validators\Validator;
+
+class ValidatorOverride extends Validator
+{
+  public function __construct($message = "", array $options = [])
+  {
+  }
+
+}
+
+class ValidatorTest extends \PHPUnit_Framework_TestCase
+{
+
+  /**
+   * @expectedException \WTForms\Exceptions\NotImplemented
+   * @expectedExceptionMessage Validator must have an overridden __construct method
+   */
+  public function testNotOverriddenConstructor()
+  {
+    new Validator;
+  }
+
+  /**
+   * @expectedException \WTForms\Exceptions\NotImplemented
+   * @expectedExceptionMessage Validator must have an overridden __invoke method
+   */
+  public function testNotOverriddenInvoke()
+  {
+    (new ValidatorOverride)->__invoke(new Form(), new DummyField());
+  }
+}
