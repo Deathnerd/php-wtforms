f38bddee91b651e27212c51efc53e47580bda402
Wes Gilleland <wes.gilleland@gmail.com>
2016-05-30 18:23:24 -0400

    RadioField 100% coverage

--- src/fields/core/SelectFieldBase.php
+++ src/fields/core/SelectFieldBase.php
@@ -82,6 +82,10 @@ class _Option extends Field
 
   public function __get($name)
   {
-    return $this->data;
+    if (is_bool($this->data)) {
+      return $this->data ? "true" : "false";
+    }
+
+    return strval($this->data);
   }
 }
